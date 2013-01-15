ebf634664551992df2d3ec92cd77345771d1fcd1
Wai Lee Chin Feman <wchinfeman@8thlight.com>
2013-01-15 08:47:06 -0600

    WIPWIPWIP

--- src/solve/core.clj
+++ src/solve/core.clj
@@ -1,20 +1,61 @@
 (ns solve.core)
 
-(declare solve-equation)
+(declare solve-equation solve-futures new-expression futures-for solver-for compact)
 
-(defn- compact [to-compact]
-  (keep identity to-compact))
+(defprotocol Expression
+  (operator [this])
+  (arguments [this])
+  (evaluate [this])
+  
+  )
 
-(defn- solver-for [variable]
-  #(solve-equation variable %))
+(defn- all-numeric? [to-check-numericality]
+  (every? number? to-check-numericality))
 
-(defn- futures-for [equation]
-  (let [lookup
-        {
-          '(= (* 2 x) 8) ['(= x 4)]
-          '(= (* 3 x) 9) ['(= x 3)]
-        }]
-    (lookup equation)))
+(defprotocol Solvable
+  (solve-for [this target-variable]))
+
+(defrecord Leaf [leaf]
+  Expression
+  (evaluate [this] leaf))
+
+(defrecord MathSyntaxTree [s-expression]
+  Expression
+  (operator [this] (if (seq? s-expression)
+                     (first s-expression)
+                     'identity))
+  (arguments [this] (if (seq? s-expression)
+                      (rest s-expression)
+                      (list s-expression)))
+  (evaluate [this] (if (all-numeric? (arguments this))
+                     (new-expression (apply (eval (operator this)) (arguments this)))
+                     this
+                     )
+    
+    )
+  )
+
+(defrecord MathEquation [lhs rhs]
+  Expression
+  (operator [this] '=)
+  (arguments [this] (list lhs rhs))
+  Solvable
+  (solve-for  [this target-variable] (solve-equation (list '= lhs rhs) target-variable)))
+
+(defn solve-equation [equation variable]
+  (let [[equals lhs rhs] equation]
+    (if (= lhs (new-expression variable))
+      rhs
+      (solve-futures variable equation)))
+  )
+
+(defn simplify [expression]
+  (if (number? expression)
+    expression
+    (let [simplified-args (map simplify (arguments expression))]
+      (if (every? number? simplified-args)
+        (evaluate expression)
+        expression))))
 
 (defn solve-futures [variable equation]
   (let [futures (futures-for equation)
@@ -25,15 +66,96 @@
 
   )
 
-(defn solve-equation [variable equation]
-  (let [[equals lhs rhs] equation]
-    (if (= lhs variable)
-      rhs
-      (solve-futures variable equation)))
-  )
+;(defmacro must-be-seq [arg & exprs]
+;  `(if (seq? ~arg)
+;    (do ~@exprs)
+;    ~arg))
 
+(def operator-opposites 
+  {
+   '+ '-
+   '- '+
+   '* '/
+   '/ '*
+  })
 
-(defn simplify [expression]
-  'x
-  )
+(defn opposite-operators? [first-operator second-operator]
+  (= (operator-opposites first-operator) second-operator))
 
+(defn new-leaf [leaf]
+  (Leaf. leaf))
+
+(defn new-expression [s-expression]
+  (if (seq? s-expression)
+    (MathSyntaxTree. (map #(if (seq? %)
+                             (new-expression %)
+                             (new-leaf %))
+                          s-expression))
+    (MathSyntaxTree. s-expression)))
+
+(defn new-equation [lhs rhs]
+  (MathEquation. (new-expression lhs) (new-expression rhs)))
+
+(defn- compact [to-compact]
+  (keep identity to-compact))
+
+(defn- solver-for [variable]
+  #(solve-equation % variable))
+
+(defn- futures-for [equation]
+  (let [lookup
+        {
+          '(= (* 2 x) 8) ['(= x 4)]
+          '(= (* 3 x) 9) ['(= x 3)]
+        }]
+
+    (lookup equation)))
+
+;
+;
+;(defn simplify-division-identity [expression]
+;  (must-be-seq expression
+;    (let [[op first-arg second-arg] expression]
+;      (if (and (= op '/) (= 1 second-arg))
+;        first-arg
+;        expression))))
+;
+;(defn- only-non-1-element [to-search]
+;  (let [filtered (filter #(not (= % 1)) to-search)]
+;    (if (= 1 (count filtered))
+;      (first filtered))))
+;
+;(defn simplify-multiplicative-identity [expression]
+;  (must-be-seq expression
+;    (let [[op first-arg second-arg] expression]
+;      (if (and (= op '*) (only-non-1-element (rest expression)))
+;        (only-non-1-element (rest expression))
+;        expression))))
+;
+;(defn mult-cancelable? [expression]
+;  (= expression '(/ (* 2 x) 2)))
+;
+;(defn get-scalar [expression]
+;  (if (seq? expression)
+;    (first (filter number? expression))
+;    expression))
+;
+;(defn simplify-division-cancel [expression]
+;  (if (mult-cancelable? expression)
+;    (let [ [op first-arg second-arg] expression
+;           first-scalar (get-scalar first-arg)
+;           second-scalar (get-scalar second-arg)
+;           new-scalar (/ first-scalar second-scalar)
+;          ]
+;      (list '/ 'x new-scalar)
+;    )
+;    expression
+;    ))
+;
+;(defn simplify-identity [expression]
+;  (-> expression simplify-multiplicative-identity simplify-division-identity))
+;
+;(defn simplify [expression]
+;  (-> expression simplify-division-cancel simplify-identity)
+;  )
+;
--- test/solve/core_test.clj
+++ test/solve/core_test.clj
@@ -2,35 +2,89 @@
   (:use clojure.test
         solve.core))
 
+(deftest data-structures
+   (let [expression (new-expression '(+ 1 2))]
+     (is (= '+ (operator expression)))
+     (is (= '(1 2) (arguments expression)))
+     )
+
+   (let [unary (new-expression 2)]
+     (is (= 'identity (operator unary)))
+     (is (= '(2) (arguments unary))))
+
+   (let [complex (new-expression '(+ (* 4 3) 2))]
+     (is (= '+ (operator complex)))
+     (is (= 2 (last (arguments complex))))
+     (let [first-argument (first (arguments complex))]
+       (is (= (new-expression '(* 4 3)) first-argument))
+       (is (= '* (operator first-argument)))
+       (is (= '(4 3) (arguments first-argument)))
+       )
+   )
+
+   (is (opposite-operators? '+ '-))
+   (is (opposite-operators? '- '+))
+   (is (opposite-operators? '* '/))
+   (is (not (opposite-operators? '* '+)))
+   )
+
 (deftest one-variable
-  (is (= 4 (solve-equation
-             'x
-             '(= x 4)
-           )))
-  (is (= 8 (solve-equation
-             'x
-             '(= x 8)
-           )))
-  (is (= 4 (solve-equation
-             'x
-             '(= (* 2 x) 8)
-            )))
-  (is (= 3 (solve-equation
-             'x
-             '(= (* 3 x) 9)
-            )))
-  (is (= 3 (solve-equation
-             'x
-             '(= (* x 3) 9)
-            )))
- )
-
-(deftest simplification
-  (is (= 'x (simplify
-              '(/ 2 (* 2 x))
-      )))
-  (is (= 'x (simplify
-              '(* 2 (/ x 2))
-      )))
-
+  (let [equation (new-equation 'x 4)]
+    (is (= (new-expression 4) (solve-for
+               equation
+               'x
+              ))))
+         ;goal
+;  (let [equation (new-equation '(* 4 x) 16)]
+;    (is (= (new-expression 4) (solve-for
+;               equation
+;               'x
+;              ))))
+         
          )
+(deftest simplification
+  (is (= 4 (simplify (new-expression '(* 2 2)))))
+  (let [expr (new-expression '(* 2 x))]
+    (is (= expr (simplify expr))))
+  (is (= 8 (simplify (new-expression '(* 4 (/ 4 2))))))
+         )
+;  (is (= 'x
+;         (simplify
+;           '(* 2 (/ x 2))
+;      )))
+;  (is (= 'x
+;         (simplify
+;           '(/ (* 2 x) 2)
+;      )))
+;
+;  (is (= '(/ x 2)
+;         (simplify
+;           '(* 2 (/ x 4))
+;      )))
+;  (is (= '(* x 2)
+;         (simplify
+;           '(/ (* x 4) 2)
+;      )))
+;  (is (= 3 (solve-equation
+;             'x
+;             '(= (* 3 x) 9)
+;            )))
+;;  (is (= 3 (solve-equation
+;;             'x
+;;             '(= (* x 3) 9)
+;;            )))
+ ;)
+;
+;
+; ; (is (= 'x (simplify '(* 1 x))))
+; (is (= '(/ x 1) (simplify-multiplicative-identity '(/ x 1))))
+; (is (= '(* x 4) (simplify-multiplicative-identity '(* x 4))))
+; (is (= 'x (simplify-multiplicative-identity '(* 1 x))))
+;
+; (is (= 22 (simplify-division-identity 22)))
+; (is (= 'x (simplify-division-identity '(/ x 1))))
+;
+; (is (= 'x (simplify '(/ x 1))))
+; (is (= 'x (simplify '(* x 1))))
+; (is (= '(1 2 3) (simplify '(1 2 3))))
+;         )
